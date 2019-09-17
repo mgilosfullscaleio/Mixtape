fa97b77a0cb72a99923f2fdcb119d54d8bf50afb
Michael Gilos <gilosmichael@gmail.com>
2019-09-17 14:44:22 +0800

    comment firebase usage

--- App/Services/FirestoreModule.js
+++ App/Services/FirestoreModule.js
@@ -1,8 +1,8 @@
-import firebase from 'react-native-firebase'
+// import firebase from 'react-native-firebase'
 import Result from 'folktale/result'
 
-const firestore = firebase.firestore()
-const auth = firebase.auth()
+// const firestore = firebase.firestore()
+// const auth = firebase.auth()
 
 const getData = () => Promise.resolve(Result.Ok())
 
