845975b429e10974d658c3aff5a3f4f1068d3909
Michael Gilos <gilosmichael@gmail.com>
2019-09-17 10:07:08 +0800

    add auth reference

--- App/Services/FirestoreModule.js
+++ App/Services/FirestoreModule.js
@@ -2,6 +2,7 @@ import firebase from 'react-native-firebase'
 import Result from 'folktale/result'
 
 const firestore = firebase.firestore()
+const auth = firebase.auth()
 
 const getData = () => Promise.resolve(Result.Ok())
 
