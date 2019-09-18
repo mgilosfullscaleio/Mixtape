54a871624a11d676c98d953fa5f726e651a2a243
Michael Gilos <gilosmichael@gmail.com>
2019-09-18 13:00:05 +0800

    working setup for firebase

--- App/Services/FirestoreModule.js
+++ App/Services/FirestoreModule.js
@@ -1,10 +1,20 @@
-// import firebase from 'react-native-firebase'
+import firebase from 'react-native-firebase'
 import Result from 'folktale/result'
 
-// const firestore = firebase.firestore()
+const firestore = firebase.firestore()
 // const auth = firebase.auth()
 
 const getData = () => Promise.resolve(Result.Ok())
+
+firebase.auth().signInAnonymously()
+	.then(()=> {
+    console.tron.log('signInAnonymously success')
+		firebase.firestore().collection('cards').doc('0JC9ULagFVdlw0jlH9Go').get().then(doc => doc.data()).then(data => console.tron.log('fs', data))
+	}).catch((err)=>{
+		console.tron.log('fs err', err)
+  })
+  
+
 
 export default {
   getData
--- android/app/src/main/java/com/mixtape/MainApplication.java
+++ android/app/src/main/java/com/mixtape/MainApplication.java
@@ -14,6 +14,9 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import io.invertase.firebase.firestore.RNFirebaseFirestorePackage; 
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -29,7 +32,8 @@ public class MainApplication extends Application implements ReactApplication {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
+      packages.add(new RNFirebaseFirestorePackage());
+      packages.add(new RNFirebaseAuthPackage());
       return packages;
     }
 
