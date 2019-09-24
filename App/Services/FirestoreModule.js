739edceb3d8facaa6fed2e5d9d3a1cd2c263909c
Michael Gilos <gilosmichael@gmail.com>
2019-09-24 13:12:06 +0800

    add firestore samples

--- App/Services/FirestoreModule.js
+++ App/Services/FirestoreModule.js
@@ -2,20 +2,70 @@ import firebase from 'react-native-firebase'
 import Result from 'folktale/result'
 
 const firestore = firebase.firestore()
-// const auth = firebase.auth()
+const auth = firebase.auth()
+const Timestamp = firebase.firestore.Timestamp
+const USER = 'usersV2'
 
-const getData = () => Promise.resolve(Result.Ok())
+const signIn = async () => await auth.signInAnonymously()
+signIn()
 
-firebase.auth().signInAnonymously()
-	.then(()=> {
-    console.tron.log('signInAnonymously success')
-		firebase.firestore().collection('cards').doc('0JC9ULagFVdlw0jlH9Go').get().then(doc => doc.data()).then(data => console.tron.log('fs', data))
-	}).catch((err)=>{
-		console.tron.log('fs err', err)
-  })
-  
 
+const createUser = info => {
+	const ref = firestore.collection(USER).doc()
+	const userData = {
+		id: ref.id,
+		created: Timestamp.now(),
+		...info,
+	}
+
+	console.tron.log('createUser', userData)
+
+	return ref.set(userData)
+		.then(() => Result.Ok(userData))
+		.catch(e => Result.Error(e))
+}
+
+createUser({
+	name: `john${Date.now()}`
+})
+
+/*
+const findUser = id => {
+  return firebase
+    .firestore()
+    .collection('users')
+    .where('social.spotify.id', '==', id)
+		.get()
+		.then(snapshot => snapshot.docs)
+		.then(docs => docs.map(doc => doc.data()))
+    .catch(error => Result.Error('Cant find user with id'))
+}
+
+const updateUser = user =>
+  firestore
+    .collection('users')
+    .doc(user.id)
+    .update(user)
+    .then(() => Result.Ok(user))
+    .catch(error => Result.Error(`Error updating bet\n${error}`))
+
+const deleteUser = user =>
+  firestore
+    .collection('users')
+    .doc(user.id)
+    .delete()
+    .then(() => Result.Ok(user))
+		.catch(error => Result.Error(`Error deleting bet\n${error}`))
+
+const getUsers = () => 
+  firestore
+    .collection('users')
+    .get()
+    .then(snapshot => snapshot.docs)
+    .then(docs => docs.map(doc => doc.data()))
+    .then(docs => Result.Ok(docs))
+*/
 
 export default {
-  getData
+	createUser
 }
