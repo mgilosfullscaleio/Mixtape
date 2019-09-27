12c84525456a47348fa5bf8b4382802638211b14
Michael Gilos <gilosmichael@gmail.com>
2019-09-27 10:02:25 +0800

    return empty function as unsubscribe

--- App/Services/FixtureFirestoreModule.js
+++ App/Services/FixtureFirestoreModule.js
@@ -4,7 +4,11 @@ export default {
   getUser: spotifyId => Promise.resolve(Result.Ok(require('../Fixtures/user.json'))),
   fetchUserInOpenMatch: (playerId) => Promise.resolve(Result.Ok(true)),
   quitOpenMatch: () => Promise.resolve(Result.Ok(true)),
-  playerJoinObserver: emitter => setTimeout(() => {
-                        emitter(require('../Fixtures/player.json'))
-                      }, 2000)
+  playerJoinObserver: emitter => () => {
+    setTimeout(() => {
+      emitter(require('../Fixtures/player.json'))
+    }, 2000)
+
+    return () => {}
+  }
 }
