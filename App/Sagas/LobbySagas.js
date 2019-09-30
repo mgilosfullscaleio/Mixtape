b68e5d6b36bf15b5a2e5f46dc9190a26cf7b7bf8
Michael Gilos <gilosmichael@gmail.com>
2019-09-30 08:28:32 +0800

    remove unused code

--- App/Sagas/LobbySagas.js
+++ App/Sagas/LobbySagas.js
@@ -3,10 +3,9 @@ import Actions, { LobbyTypes } from '../Redux/LobbyRedux'
 import { NavigationActions } from 'react-navigation'
 import { eventChannel } from 'redux-saga'
 
-//TODO consider having an action for quitting lobby, for event channel saga to take and close
 export function * quitOpenMatch (api, action) {
   yield put(Actions.unsubscribeOpenMatchUpdates())
-  
+
   yield call(api.removePlayerFromOpenMatch)
 
   yield put(NavigationActions.navigate({ routeName: 'Home' }))
@@ -22,12 +21,6 @@ const onPlayerJoinChannel = firestore =>
 export function * subscribePlayerJoin (firestore, action) {
   const channel = yield call(onPlayerJoinChannel, firestore)
 
-  // while (true) {
-  //   const player = yield take(playerJoinSubscriptionChannel)
-
-  //   yield put(Actions.playerJoinMatch(player))
-  // }
-
   yield takeEvery(channel, function * (payload) {
     yield put(Actions.playerJoinMatch(payload))
   })
@@ -36,13 +29,6 @@ export function * subscribePlayerJoin (firestore, action) {
   yield take(LobbyTypes.UNSUBSCRIBE_OPEN_MATCH_UPDATES)
   channel.close()
 }
-
-// export function * unsubscribeOpenMatchUpdates (action) {
-//   if (playerJoinSubscriptionChannel) {
-//     playerJoinSubscriptionChannel.close()
-//     playerJoinSubscriptionChannel = null
-//   }
-// }
 
 export function * addPlayerInMatch (api, { playerId }) {
   yield call(api.addPlayerToOpenMatch, playerId)
