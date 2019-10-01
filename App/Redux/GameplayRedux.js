fd62a062f34b8c54df2e864aba95dbe261ed7d8f
Michael Gilos <gilosmichael@gmail.com>
2019-10-01 08:20:00 +0800

    remove unused code and add gameId to state

--- App/Redux/GameplayRedux.js
+++ App/Redux/GameplayRedux.js
@@ -26,11 +26,9 @@ export default Creators
 
 export const INITIAL_STATE = Immutable({
   round: 2,
-  data: null,
   fetching: null,
-  payload: null,
   error: null,
-
+  gameId: null,
   searchedSongs: [],
 })
 
@@ -43,23 +41,11 @@ export const GameplaySelectors = {
 
 /* ------------- Reducers ------------- */
 
-// request the data from an api
-export const request = (state, { data }) =>
-  state.merge({ fetching: true, data, payload: null })
-
-// successful api lookup
-export const success = (state, action) => {
-  const { payload } = action
-  return state.merge({ fetching: false, error: null, payload })
-}
-
-// Something went wrong somewhere.
 export const failure = state =>
   state.merge({ fetching: false, error: true, payload: null })
 
-export const saveSongSelectionSuccess = (state, { song }) => {
+export const saveSongSelectionSuccess = (state, { song }) =>
   state.merge({ fetching: false, error: true, song })
-}
 
 export const voteRoundWinnerSuccess = state =>
   state.merge({ fetching: false, error: false, payload: null })
@@ -73,8 +59,6 @@ export const saveGameId = (state, { gameId }) =>
 /* ------------- Hookup Reducers To Types ------------- */
 
 export const reducer = createReducer(INITIAL_STATE, {
-  [Types.GAMEPLAY_REQUEST]: request,
-  [Types.GAMEPLAY_SUCCESS]: success,
   [Types.GAMEPLAY_FAILURE]: failure,
   [Types.SAVE_SONG_SELECTION_SUCCESS]: saveSongSelectionSuccess,
   [Types.VOTE_ROUND_WINNER_SUCCESS]: voteRoundWinnerSuccess,
