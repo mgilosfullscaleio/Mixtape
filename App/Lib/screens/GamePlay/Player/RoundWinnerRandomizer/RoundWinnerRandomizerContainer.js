142ede59160ee375cee12985e8f14e5a576b3c70
Michael Gilos <gilosmichael@gmail.com>
2019-10-09 13:30:31 +0800

    connect container to the gameplay redux

--- App/Lib/screens/GamePlay/Player/RoundWinnerRandomizer/RoundWinnerRandomizerContainer.js
+++ App/Lib/screens/GamePlay/Player/RoundWinnerRandomizer/RoundWinnerRandomizerContainer.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import RoundWinnerRandomizer from './RoundWinnerRandomizer';
-
+import { connect } from 'react-redux'
+import GameplayActions, { GameplaySelectors } from '../../../../../Redux/GameplayRedux';
 import { mockData } from '../../../../constants';
 
-const RoundWinnerRandomizerContainer = () => (
+const RoundWinnerRandomizerContainer = props => (
   <RoundWinnerRandomizer
     songs={mockData.songs}
     players={mockData.playersInGame}
-    scenario={mockData.scenario}
+    scenario={props.selectCardContent}
   />
 );
 
-export default RoundWinnerRandomizerContainer;
+const mapStateToProps = (state) => ({
+  selectCardContent: GameplaySelectors.selectCardContent(state),
+})
+ 
+const mapDispatchToProps = (dispatch) => ({
+})
+ 
+export default connect(mapStateToProps, mapDispatchToProps)(RoundWinnerRandomizerContainer)
