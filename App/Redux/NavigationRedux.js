1d50c0845b508a5be0183bef428b9337941eb12f
Michael Gilos <gilosmichael@gmail.com>
2019-10-11 10:28:21 +0800

    add selector to navigation

--- App/Redux/NavigationRedux.js
+++ App/Redux/NavigationRedux.js
@@ -1,5 +1,24 @@
 import AppNavigation from '../Navigation/AppNavigation'
 
+/*
+routes: [
+  {
+    isTransitioning:false
+    index:0
+    key:AUTH_LOGIN
+    routeName:AUTH_LOGIN
+    params:undefined
+  }
+],
+index:0,
+isTransitioning:false
+*/
+
+export const NavigationSelectors = {
+  selectIndex: state => state.nav.index,
+  selectCurrentRoute: state => state.nav.routes[state.nav.index]
+}
+
 export const reducer = (state, action) => {
   const newState = AppNavigation.router.getStateForAction(action, state)
   return newState || state
